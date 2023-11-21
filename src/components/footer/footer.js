2e25123773b8cc0e9512e64b3c50f9fd092d3c87
Gurudev Y <gurudev96@gmail.com>
2023-11-21 15:24:06 -0800

    added new components

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from "footer.css";
+
+
+const footer = (props) => {
+
+    return (
+        <div>
+            SOme JSZ
+        </div>
+    )
+}
+
+export default footer;
